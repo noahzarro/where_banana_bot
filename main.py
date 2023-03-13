b6d2ee68a6fd1853ad03e3f34222b155bb0aa552
Pascal Schärli <github@pascscha.ch>
2023-03-13 09:13:40 +0100

    Autoformatting

--- main.py
+++ main.py
@@ -12,42 +12,45 @@ import os
 TOKEN = os.environ.get("TOKEN")
 
 # create Bot
-#with open("token.json","r") as read_file:
+# with open("token.json","r") as read_file:
 #    TOKEN = json.load(read_file)["token"]
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 myFont = ImageFont.truetype("Ubuntu-Bold.ttf", 20)
 
+
 def create_image(what: str):
-    img = Image.open('template.jpg')
+    img = Image.open("template.jpg")
     I1 = ImageDraw.Draw(img)
-    to_draw = "where "+what
-    #to_draw = "a"
-    I1.text((580-len(to_draw)*4, 45), to_draw, fill=(20, 20, 20), font=myFont)
+    to_draw = "where " + what
+    # to_draw = "a"
+    I1.text((580 - len(to_draw) * 4, 45), to_draw, fill=(20, 20, 20), font=myFont)
 
     img.save("temp.png")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Aaaagi! Use /where <word> to generate an image"
+        text="Aaaagi! Use /where <word> to generate an image",
     )
+
 
 async def where(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if len(context.args) > 0:
-        what = ' '.join(context.args)
+        what = " ".join(context.args)
         create_image(what)
-        with open("temp.png", 'rb') as img_to_send:
+        with open("temp.png", "rb") as img_to_send:
             await context.bot.send_photo(
-                chat_id=update.effective_chat.id,
-                photo=img_to_send
+                chat_id=update.effective_chat.id, photo=img_to_send
             )
     else:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Aaaagi! Use /where <word> to generate an image"
+            text="Aaaagi! Use /where <word> to generate an image",
         )
+
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("where", where))
